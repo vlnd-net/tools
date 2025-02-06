e2e3b5b7943cb337628c348be05215ff23e4f178
vlnd-net <115617583+vlnd-net@users.noreply.github.com>
2025-02-06 16:08:10 +0000

    Update update_cleanup_menu.sh

--- update_cleanup_menu.sh
+++ update_cleanup_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_URL="https://raw.githubusercontent.com/vlnd-net/tools/f763eff7230e06a31980b6aee279b5a70cd3540a/update_cleanup_menu.sh"
+REPO_URL="https://raw.githubusercontent.com/vlnd-net/tools/refs/heads/main/update_cleanup_menu.sh"
 LOCAL_SCRIPT="$0"
 
 check_update() {
